a00142f36e509611712ada195cbbe87eab292f6e
Alejandro0120 <alejandrogd@alumnos.iesgalileo.es>
2022-12-10 21:12:45 +0100

    deploy index2

--- CCGWold/settings.py
+++ CCGWold/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
